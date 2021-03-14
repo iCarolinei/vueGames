09e31f44723bd3896a0830b26d5be247a15ec4f8
Caroline <caroline-jan@outlook.com>
2021-03-14 13:01:53 +0100

    font awesome

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGamepad } from '@fortawesome/free-solid-svg-icons';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faGamepad);
 library.add(faAngleLeft);
+
+library.add(faHeart);
 
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -5,9 +5,22 @@
       'background-image': 'url(' + require('../assets/' + activeImg) + ')',
     }"
   >
-    <div class="overlay-gradient">
+    <div :class="'overlay-gradient ' + profile.style">
       <div class="back">
         <font-awesome-icon icon="angle-left" @click="closeProfile" />
+      </div>
+      <div class="actions">
+        <font-awesome-icon
+          :icon="['far', 'heart']"
+          v-if="!profile.like"
+          @click="profile.like = !profile.like"
+        />
+        <font-awesome-icon
+          :icon="['fas', 'heart']"
+          class="red"
+          v-if="profile.like"
+          @click="profile.like = !profile.like"
+        />
       </div>
     </div>
   </div>
@@ -39,5 +52,71 @@ export default {
   height: 100%;
   background-position: top center;
   background-size: cover;
+  transition: all 0.4s linear;
+  .overlay-gradient {
+    width: 100%;
+    height: calc(100% - 20px);
+    padding-top: 20px;
+  }
+  .fps1 {
+    background: linear-gradient(
+      to bottom,
+      rgba($color: #fff, $alpha: 0) 30%,
+      rgba($color: #845ec2, $alpha: 1) 80%,
+      rgba($color: #845ec2, $alpha: 1) 100%
+    );
+  }
+  .fps2 {
+    background: linear-gradient(
+      to bottom,
+      rgba($color: #fff, $alpha: 0) 30%,
+      rgba($color: #d65db1, $alpha: 1) 80%,
+      rgba($color: #d65db1, $alpha: 1) 100%
+    );
+  }
+  .fps3 {
+    background: linear-gradient(
+      to bottom,
+      rgba($color: #fff, $alpha: 0) 30%,
+      rgba($color: #ff6f91, $alpha: 1) 80%,
+      rgba($color: #ff6f91, $alpha: 1) 100%
+    );
+  }
+  .fps4 {
+    background: linear-gradient(
+      to bottom,
+      rgba($color: #fff, $alpha: 0) 30%,
+      rgba($color: #ff9671, $alpha: 1) 80%,
+      rgba($color: #ff9671, $alpha: 1) 100%
+    );
+  }
+  .back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 20px;
+
+    .angle-left {
+      font-size: 4rem;
+      cursor: pointer;
+      color: white;
+    }
+  }
+  .actions {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 25px 20px;
+
+    .red {
+      color: red;
+    }
+
+    .heart {
+      font-size: 3rem;
+      cursor: pointer;
+      color: white;
+    }
+  }
 }
 </style>
